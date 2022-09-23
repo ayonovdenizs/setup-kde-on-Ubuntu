6993ed2d42cdf164f1d5b674dbbb882d475c519d
Denis <77270222+AyonovDenizs@users.noreply.github.com>
2022-09-23 14:22:45 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
+clear
 sudo apt update
+clear
 sudo apt full-upgrade
+clear
 sudo apt install tasksel
 sudo tasksel install kubuntu-full
+clear
 sudo apt install xfce4 xfce4-goodies
 sudo apt install tightvncserver
+clear
 vncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
@@ -11,3 +16,6 @@ wget https://raw.githubusercontent.com/AyonovDenizs/setuping435/ayden/xstartup
 mv xstartup ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
 vncserver -localhost
+echo "успешно установлено."
+echo "Successfully installed."
+
